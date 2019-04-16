741463d2fbc8813debfb5773d153c780df792852
Jeroen D Stout <jeroenstout@gmail.com>
2019-04-16 20:53:04 +0200

    Improve git branch finding method

--- util_fixsubmodules.py
+++ util_fixsubmodules.py
@@ -2,6 +2,17 @@ import os
 import sys
 import git
 
+    # This is the priority of our branch names
+    # in descending order
+branch_name_priority = [
+    "public-master",
+    "public-develop",
+    "private-master",
+    "private-develop",
+    "-master",
+    "-develop"
+]
+
     # Find our startup path; by default our path + "../../"
     # This usually corresponds to the main git directory
 dir_path = os.path.dirname(sys.argv[0]) + "/../../"
@@ -37,51 +48,71 @@ for dir in git_dirs:
         print()
         continue
     
-    match = 0
-    matchMaster = 0
-    matchMasterDet = 0
-    matchDevelop = 0
-    matchDevelopDet = 0
-    matchNames = []
+    matchHead       = 0
+    matchQuality    = 999
     
         # Compare the hexsha against branches;
         # we favour the main master/develop, and
         # then favour relative master/develop
     hex = repo.head.commit.hexsha
     
+    matchNames = []
     for head in repo.heads:
         if head.commit.hexsha != hex:
             continue
         matchNames.append(head.name)
-        if head.name == "public-master":
-            matchMaster = head
-        if head.name == "public-develop":
-            matchDevelop = head
-        if head.name.endswith("-master"):
-            matchMasterDet = head
-        if head.name.endswith("-develop"):
-            matchDevelopDet = head
+        for num, name in enumerate(branch_name_priority):
+            if matchQuality <= num:
+                continue
+            matchHead = head
+            matchQuality = num
         
-    match = matchMaster
-    if not match:
-        match = matchDevelop
-    if not match:
-        match = matchMasterDet
-    if not match:
-        match = matchDevelopDet
-        
-        # Print heads and correct, or complain we have no branch
-    if len(matchNames) > 0:
+        # If we found a head, print all; marking the one we selected
+    if matchHead != 0:
         print("Detached head matching")
         for matchNm in matchNames:
-            if matchNm == match.name:
+            if matchNm == matchHead:
                 print(" - " + matchNm + " *")
             else:
                 print(" - " + matchNm)
-        repo.head.reference = match
-    else:
-        print("Detached head with no matching branches - no changes")
+        repo.head.reference = matchHead
+        print()
+        continue
+        
+    print("Detached head with no matching branches, fetching...")
     
+        # Fetch from the repo's remotes to see if any repote branches
+        # match our pattern
+    matchNames = []
+    for remote in repo.remotes:
+        for fetch_info in remote.fetch():
+            if fetch_info.commit.hexsha != hex:
+                continue
+            matchNames.append(fetch_info.ref.name)
+            for num, name in enumerate(branch_name_priority):
+                if matchQuality <= num:
+                    continue
+                matchHead = fetch_info.ref.name
+                matchQuality = num
+    
+        # If we found a match, create a local head with that name;
+        # we again print all with the selected one marked
+    if matchHead != 0:
+        print("Detached head matching")
+        for matchNm in matchNames:
+            if matchNm == matchHead:
+                print(" - " + matchNm + " *")
+            else:
+                print(" - " + matchNm)
+                
+        branchName = matchHead.split('/', 1)[-1]
+        new_head = repo.create_head(branchName)
+        repo.head.set_reference(new_head)
+                
+        print()
+        continue
+        
+    print("No matches with remotes! This is truly a HEAD.")
     print()
     
 print("Done.")
