b3f4745807ae50d69bf7682abd4fc9844bee90da
Jeroen D Stout <jeroenstout@gmail.com>
2019-04-20 16:35:04 +0200

    Add pnpm installer

---
+++ util_pnpm.py
@@ -0,0 +1,38 @@
+print("Checking pnpm...\r", end='')
+
+import os
+import shared_choice
+import sys
+import json
+import subprocess
+
+    # Find our requirements json, by default or by argument
+reqJSONPath = os.path.dirname(sys.argv[0]) + "/../requirements.json"
+if len(sys.argv) > 1:
+    reqJSONPath = sys.argv[1]
+
+    # Find our base path, by default or by argument
+basePath = os.path.dirname(sys.argv[0]) + "/../../"
+if len(sys.argv) > 2:
+    reqJSONPath = sys.argv[2]
+
+    # Parse JSON and find pipReq
+requirements = []
+with open(reqJSONPath, 'r') as f:
+    requirements = json.load(f)
+pnpmReq = requirements["pnpm"]
+    
+print("This script would like to use pnpm to install in:")
+for req in pnpmReq:
+    print(" * " + req["install"])
+print()
+
+if False == shared_choice.yn("Is using pnpm OK?"):
+    sys.exit()
+print()
+   
+for req in pnpmReq:
+    path = basePath + req["install"];
+    print("[" + req["install"] + "]")
+    proc = subprocess.Popen(["pnpm", "install", "-D"], cwd=path, shell=True)
+    proc.wait()
